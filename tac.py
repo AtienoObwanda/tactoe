14b97897ef91ad4ee6f29d0042c0f1a0ee4e64aa
atienoobwanda <atienoobwanda@gmail.com>
2022-04-23 11:11:56 +0300

    Added the game layout

--- tac.py
+++ tac.py
@@ -9,3 +9,17 @@ currentPlayer = "X"
 winner= None
 gameRunning = True
 
+#Print the game board
+def printGame(board):
+    print("*********")
+   
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("---------")
+
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("---------")
+
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print("*********")
+
+printGame(board)
